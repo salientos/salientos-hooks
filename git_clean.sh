9ab00d0dba5319323e4be688ee83c1156e205864
salientos <d3signr@gmail.com>
2021-05-15 10:12:10 +0100

    Update Build

--- git_clean.sh
+++ git_clean.sh
@@ -2,7 +2,7 @@
 
 ##############################################################################
 #
-#  Program :	Clean_Up v1
+#  Program :	clean_up v1
 #  Arch    :	x86_64 
 #  Author  : 	Silent Robot
 #  Website : 	https://sourceforge.net/projects/salient-os/
@@ -10,22 +10,34 @@
 ##############################################################################
 
 _msg() {
-	term_cols=$(tput cols) 
-	str=":: $1 ::"
-	for ((i=1; i<=`tput cols`; i++)); do echo -n ‾; done
-	tput setaf 10; printf "%*s\n" $(((${#str}+$term_cols)/2)) "$str"; tput sgr0
-	for ((i=1; i<=`tput cols`; i++)); do echo -n _; done
-	printf "\n"
+    printf "\n"
+    term_cols=$(tput cols)
+    str="$1"
+    tput setaf 5; printf '%*s\n' "${term_cols}" '' | tr ' ' - ; tput sgr0
+    tput setaf 4; printf "%*s\n" $(((${#str}+$term_cols)/2)) "$str"; tput sgr0
+    tput setaf 5; printf '%*s\n' "${term_cols}" '' | tr ' ' - ; tput sgr0
 }
 
-_msg "Cleaning up repo..."
-
+# Backup config & remove .git dir
+_msg "Backing up config & deleting .git dir..."
 mv .git/config config
 rm -rf .git
-sh ./git_setup*
+
+# Reinitialise git
+_msg "Setting up git repository..."
+git init
+git config --global user.name "salientos"
+git config --global user.email "d3signr@gmail.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+_msg "Git Repository Setup Complete!"
+
+# Move config back to .git & push changes to repo
+_msg "Moving config to .git dir & pushing changes..."
 mv config .git/config
 git add --all .
-git commit -m "Initialise Repo"
+git commit -m "Repository is Online!"
 git push origin master --force
-
-_msg "Clean up completed..."
+_msg "Git clean up completed..."
--- git_push.sh
+++ git_push.sh
@@ -2,7 +2,7 @@
 
 ##############################################################################
 #
-#  Program :	Git_Push v1
+#  Program :	git_push v1
 #  Arch    :	x86_64 
 #  Author  : 	Silent Robot
 #  Website : 	https://sourceforge.net/projects/salient-os/
@@ -10,29 +10,23 @@
 ##############################################################################
 
 _msg() {
-	term_cols=$(tput cols) 
-	str=":: $1 ::"
-	for ((i=1; i<=`tput cols`; i++)); do echo -n ‾; done
-	tput setaf 10; printf "%*s\n" $(((${#str}+$term_cols)/2)) "$str"; tput sgr0
-	for ((i=1; i<=`tput cols`; i++)); do echo -n _; done
-	printf "\n"
+    printf "\n"
+    term_cols=$(tput cols)
+    str="$1"
+    tput setaf 5; printf '%*s\n' "${term_cols}" '' | tr ' ' - ; tput sgr0
+    tput setaf 4; printf "%*s\n" $(((${#str}+$term_cols)/2)) "$str"; tput sgr0
+    tput setaf 5; printf '%*s\n' "${term_cols}" '' | tr ' ' - ; tput sgr0
 }
 
 _msg "Checking for newer files online."
 git pull
-#cd x86_64
-#sh upd_repo.sh
 
 _msg "Backing up everything in project folder."
-git add --all .
+git add --all . | tee -a README.md
 
-_msg "Enter your commit message (optional)"
+read -p "Enter your commit message (optional): " input
 
-read input
-
-# Committing to the local repository with commit comment if given
-
-_msg "Committing to the local repository."
+_msg "Committing to the repository."
 git commit -m "$input"
 
 _msg "Pushing local files to Github."
